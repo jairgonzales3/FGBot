c1deeb33f896678e9dc5718fb3eca5efaa87b4a0
Jair Gonzales <108778872+jairgonzales3@users.noreply.github.com>
2022-12-07 23:45:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ import fs from 'fs'
 
 
 global.owner = [
+  ['51963003532', 'FG Bot', true],
   ['51955095498', '@𝐟𝐟𝐱𝐫𝐲𝐮.𝟏𝟖', true],
-  ['51933348947', '@𝐟𝐟𝐱𝐫𝐲𝐮.𝟏𝟖(secundario)', true],
 ] // Cambia los numeros que quieras
 global.mods = [] 
 global.prems = [] 
@@ -63,11 +63,11 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = '@𝐟𝐟𝐱𝐫𝐲𝐮.𝟏𝟖'
-global.author = '@𝐟𝐟𝐱𝐫𝐲𝐮.𝟏𝟖'
+global.packname = 'FG Bot'
+global.author = 'FG Bot'
 
-global.wm = '@𝐟𝐟𝐱𝐫𝐲𝐮.𝟏𝟖'
-global.igfg = '@𝐟𝐟𝐱𝐫𝐲𝐮.𝟏𝟖'
+global.wm = 'FG Bot'
+global.igfg = 'FG Bot'
 global.wait = '*⌛ _Cargando..._ ▬▬▬▭*'
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg')
